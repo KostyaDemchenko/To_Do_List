a50d6a601d1e724ea338c2f7b9f69509edf126cf
Jpeg <kostya.doge@gmail.com>
2024-03-10 22:42:10 +0200

    Filter fixes

--- src/components/TodoCounter/index.tsx
+++ src/components/TodoCounter/index.tsx
@@ -1,44 +1,59 @@
-// import components
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { toggleTodo } from "../../store/todoActions";
 import { Todo } from "../../store/types";
+import { Filters } from "../../store/todoActions"; // Импортируем типы фильтров
 
-// import css
 import "./style.css";
 
 const TodoCounter = () => {
   const dispatch = useDispatch();
   const todos = useSelector((state: any) => state.todos);
+  const filter = useSelector((state: any) => state.filter); // Получаем текущий выбранный фильтр
 
-  const completedTodos = todos.filter((todo: Todo) => todo.completed);
-  const uncompletedTodos = todos.filter((todo: Todo) => !todo.completed);
+  // Фильтруем задачи в соответствии с выбранным фильтром
+  const filteredTodos = todos.filter((todo: Todo) => {
+    if (filter === Filters.ALL) {
+      return true;
+    } else if (filter === Filters.COMPLETED) {
+      return todo.completed;
+    } else {
+      return !todo.completed;
+    }
+  });
+
+  const completedTodos = filteredTodos.filter((todo: Todo) => todo.completed);
+  const uncompletedTodos = filteredTodos.filter(
+    (todo: Todo) => !todo.completed
+  );
 
   return (
     <div className='todo-counter'>
-      <div className='un-complited-container'>
+      <div className='un-completed-container'>
         <hgroup>
           <h3>Uncompleted: {uncompletedTodos.length}</h3>
         </hgroup>
         {uncompletedTodos.map((todo: Todo) => (
           <div
-            className='un-complited-element'
+            className='un-completed-element'
             onClick={() => dispatch(toggleTodo(todo.id))}
+            key={todo.id} // Переместил key внутрь div
           >
-            <p key={todo.id}>{todo.content}</p>
+            <p>{todo.content}</p>
           </div>
         ))}
       </div>
-      <div className='complited-container'>
+      <div className='completed-container'>
         <hgroup>
           <h3>Completed: {completedTodos.length}</h3>
         </hgroup>
         {completedTodos.map((todo: Todo) => (
           <div
-            className='complited-element'
+            className='completed-element'
             onClick={() => dispatch(toggleTodo(todo.id))}
+            key={todo.id} // Переместил key внутрь div
           >
-            <p key={todo.id}>{todo.content}</p>
+            <p>{todo.content}</p>
           </div>
         ))}
       </div>
