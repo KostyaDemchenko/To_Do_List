003c9960112ed3c32e51831e972d0464bf76a92c
KostyaDemchenko <kostya.doge@gmail.com>
2025-01-16 18:25:33 +0200

    --fix

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+// import components
+import TodoForm from "./components/TodoForm";
+import TodoFilter from "./components/TodoFilter";
+import TodoCounter from "./components/TodoCounter";
+
+// import css
+import "./global.css";
+import "./media.css";
+
+function App() {
+  return (
+    <div className='app-wrapper'>
+      <h1>ToDo App</h1>
+      <TodoForm />
+      <TodoFilter />
+      <TodoCounter />
+    </div>
+  );
+}
+
+export default App;
